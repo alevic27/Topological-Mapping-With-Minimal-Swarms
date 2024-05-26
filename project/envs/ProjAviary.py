a13c02005b29639e1237ea8caa6f362490c701cf
GabMic <gabmic29@gmail.com>
2024-05-26 23:47:21 +0200

    mergiatura

---
+++ project/envs/ProjAviary.py
@@ -0,0 +1,412 @@
+import os
+import numpy as np
+import pybullet as p
+from gymnasium import spaces
+import pkg_resources
+
+from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
+from gym_pybullet_drones.utils.enums import DroneModel, Physics, ActionType, ImageType
+
+class ProjAviary(CtrlAviary):
+    """Multi-drone environment class for topological mapping"""
+
+    ################################################################################
+
+    def __init__(self,
+                 drone_model: DroneModel=DroneModel.CF2X,
+                 num_drones: int=1,
+                 neighbourhood_radius: float=np.inf,
+                 initial_xyzs=None,
+                 initial_rpys=None,
+                 physics: Physics=Physics.PYB,
+                 pyb_freq: int = 240,
+                 ctrl_freq: int = 240,
+                 gui=False,
+                 vision=False,
+                 save_imgs=False,
+                 obstacle_ids=False,
+                 labyrinth_id: str = "0",
+                 sensors_attributes = True,
+                 max_sensors_range: float = np.inf,
+                 ref_distance : int = 0,
+                 s_WF: int = -1,
+                 c_omega : float = 0,
+                 c_vel: float = 0,
+                 output_folder='results',
+                 img_res: np.ndarray=np.array([64, 48]),
+                 ):
+        """Initialization of an environment with drones capable of performing topological mapping.
+
+        Attributes `vision_attributes` is selected
+        based on the choice of `obs`; `obstacles` is set to True 
+        and overridden with landmarks for mapping applications
+
+        Parameters
+        ----------
+        drone_model : DroneModel, optional
+            The desired drone type (detailed in an .urdf file in folder `assets`).
+        num_drones : int, optional
+            The desired number of drones in the aviary.
+        neighbourhood_radius : float, optional
+            Radius used to compute the drones' adjacency matrix, in meters.
+        initial_xyzs: ndarray | None, optional
+            (NUM_DRONES, 3)-shaped array containing the initial XYZ position of the drones.
+        initial_rpys: ndarray | None, optional
+            (NUM_DRONES, 3)-shaped array containing the initial orientations of the drones (in radians).
+        physics : Physics, optional
+            The desired implementation of PyBullet physics/custom dynamics.
+        pyb_freq : int, optional
+            The frequency at which PyBullet steps (a multiple of ctrl_freq).
+        ctrl_freq : int, optional
+            The frequency at which the environment steps.
+        gui : bool, optional
+            Whether to use PyBullet's GUI.
+        vision : bool, optional
+            Whether to use the camera/vision sensor
+        save_imgs : bool, optional
+            Whether to save pictures.
+        obstacle_ids : list, optional
+            Select how many and which obstacles/assets to add to the simulation
+        sensors_attributes : bool, optional
+            Whether to add rangefinders to the drone
+        img_res: numpy array, optional
+            Specify the resolution of the captured images
+        Gli input aggiunti sono:
+        sensors_attributes : booleano , optional
+            mettere True se ci sono i RangeFinders
+        max_sensors_range : float
+            gittata RangeFinders (TODO CONTROLLARE SIA IN METRI)
+        ref_distance : int = 0,
+            distanza di riferimento dal muro per il WallFollowing
+        s_WF: int = -1,
+            wall following side: 1 for Right side, -1 for Left Side (TODO vedere se cambiare lato visto che le coordinate di destra sono negative)
+        c_omega : float = 0,
+            NON MI RICORDO CHE è
+        c_vel: float = 0,
+            NON MI RICORDO CHE è
+    
+        """
+
+        ####
+        self.VISION = vision
+        self.SAVE = save_imgs
+        self.OBSTACLE_IDS = obstacle_ids
+        self.LABYRINTH_ID = labyrinth_id
+        super().__init__(drone_model=drone_model,
+                         num_drones=num_drones,
+                         neighbourhood_radius=neighbourhood_radius,
+                         initial_xyzs=initial_xyzs,
+                         initial_rpys=initial_rpys,
+                         physics=physics,
+                         pyb_freq=pyb_freq,
+                         ctrl_freq=ctrl_freq,
+                         gui=gui,
+                         record=False, 
+                         obstacles=True, 
+                         user_debug_gui=True,
+                         vision_attributes=vision,
+                         output_folder=output_folder
+                         )
+        
+        #### Set the image resoution to the desired one
+        self.IMG_RES = img_res
+
+        ### Create attributes for sensors ####################
+        # Aggiungo tot sensori al drone
+        self.SENSOR_ATTR = sensors_attributes
+        self.max_range = max_sensors_range   # max_sensors_range
+        self.distWallRef = ref_distance      # distanza di riferimento dal muro
+        self.sWF = s_WF                      # wall following side: 1 for Right side, -1 for Left Side
+        self.comega = c_omega                
+        self.cvel=c_vel
+        self.NUM_SENSORS = 4
+        if self.SENSOR_ATTR :
+            self.sensor_position =np.zeros((4,3))
+            self.sensor_direction = np.array([
+                                        [1.0, 0.0, 0.0],  # front
+                                        [0.0, 1.0, 0.0],  # Lx                                        
+                                        [-1.0, 0.0, 0.0],   # behind
+                                        [0.0, -1.0, 0.0],  # Rx
+                                        ])   
+            # TODO piccolo check sul se serve inizializzare rot_mat    
+        
+    ###################################################################
+        
+    def step(self,
+             action
+             ):
+        """Advances the environment by one simulation step."""
+        # Advance the simulation like in BaseAviary
+        obs, reward, terminated, truncated, info = super().step(action)
+
+        #Add output from rangefinders 
+        obs_sensors=self._sensorsObs()
+
+        # TODO aggiungere il richiamo a _storeHitPoints(self)
+
+        return obs, obs_sensors, reward, terminated, truncated, info
+    
+    ###################################################################
+        
+    def _addObstacles(self):
+        """Add obstacles to the environment.
+
+        Only if the observation is of type RGB, landmarks are added.
+        Overrides BaseAviary's method.
+        
+        """
+        #TODO: define the logic through which we select the obstacles using the self.OBSTACLE_IDS variable
+        # for the moment the function is the same as in BaseRLAviary.py
+        if self.VISION :
+            if self.LABYRINTH_ID == "0":
+                p.loadURDF("block.urdf",
+                           [1, 0, .1],
+                           p.getQuaternionFromEuler([0, 0, 0]),
+                           physicsClientId=self.CLIENT
+                           )
+                p.loadURDF("cube_small.urdf",
+                           [0, 1, .1],
+                           p.getQuaternionFromEuler([0, 0, 0]),
+                           physicsClientId=self.CLIENT
+                           )
+                p.loadURDF("duck_vhacd.urdf",
+                           [-1, 0, .1],
+                           p.getQuaternionFromEuler([0, 0, 0]),
+                           physicsClientId=self.CLIENT
+                           )
+                p.loadURDF("teddy_vhacd.urdf",
+                           [0, -1, .1],
+                           p.getQuaternionFromEuler([0, 0, 0]),
+                           physicsClientId=self.CLIENT
+                           )
+            else:
+                p.loadURDF(pkg_resources.resource_filename('project','assets/'+'labyr'+self.LABYRINTH_ID+'.urdf'),
+                       [0, 0, 0],
+                       p.getQuaternionFromEuler([0,0,0]),
+                       useFixedBase = 1
+                       )
+        else:
+            pass
+
+    # TODO def _addDecorations(self):
+    # funzione più vasta di addObstacles che possa caricare vari dettagli che 
+    # aiutino la caratterizzazione dell'environment per il riconoscimento
+
+    ################################################################################
+        
+    def _actionSpace(self):
+        """Returns the action space of the environment.
+        Copiata uguale da CtrlAviary.py
+        Returns
+        -------
+        spaces.Box
+            An ndarray of shape (NUM_DRONES, 4) for the commanded RPMs.
+
+        """
+        #### Action vector ######## P0            P1            P2            P3
+        act_lower_bound = np.array([[0.,           0.,           0.,           0.] for i in range(self.NUM_DRONES)])
+        act_upper_bound = np.array([[self.MAX_RPM, self.MAX_RPM, self.MAX_RPM, self.MAX_RPM] for i in range(self.NUM_DRONES)])
+        return spaces.Box(low=act_lower_bound, high=act_upper_bound, dtype=np.float32)
+    
+    ################################################################################
+    
+    def _computeObs(self):
+        """Returns the current observation of the environment. 
+        If the observation type is RGB, it saves the images captured by the drones.
+        Returns
+        -------
+        ndarray
+            An ndarray of shape (NUM_DRONES, 20) with the state of each drone.
+
+        """
+        if self.VISION:
+            if self.step_counter%self.IMG_CAPTURE_FREQ == 0:
+                for i in range(self.NUM_DRONES):
+                    self.rgb[i], self.dep[i], self.seg[i] = self._getDroneImages(i,
+                                                                                 segmentation=False
+                                                                                 )
+                    #### Printing observation to PNG frames example ############
+                    if self.SAVE:
+                        self._exportImage(img_type=ImageType.RGB,
+                                          img_input=self.rgb[i],
+                                          path=self.ONBOARD_IMG_PATH+"/drone_"+str(i),
+                                          frame_num=int(self.step_counter/self.IMG_CAPTURE_FREQ)
+                                          )
+            return np.array([self._getDroneStateVector(i) for i in range(self.NUM_DRONES)])
+        else:
+            return np.array([self._getDroneStateVector(i) for i in range(self.NUM_DRONES)])
+        
+    ################################################################################
+        
+    def _sensorsObs(self,
+                    nth_drone):
+        '''Returns distance from obstacles in the 4 directions # front Rx behind Lx 
+        and cloud of up to 4 hit points
+
+        Parameters
+        ----------
+        nth_drone : int
+            The ordinal number/position of the desired drone in list self.DRONE_IDS.
+
+        Returns
+        -------
+        observation 
+            numpy array con le distanze dal primo ostacolo nelle 4 direzioni
+        Hit_point 
+            numpy array bidimensionale (lista di punti rilevati dai rangefinders)
+            TODO: sviluppare qualcosa che salva i 4 punti in Hit_point a ogni step
+        '''
+        observation = np.array([[self.max_range,self.max_range,self.max_range,self.max_range] for j in range(self.NUM_DRONES)] )
+        Hit_point = []
+        if self.SENSOR_ATTR:
+            for i in range(self.NUM_DRONES):
+                closest_hit = []
+                for j in range(self.NUM_SENSORS) :
+                    self.sensor_position = self._getDroneStateVector(i)[0:3]  # [1. 1. 1.] posizione
+                    self.rot_mat = np.array(p.getMatrixFromQuaternion(self.quat[i, :])).reshape(3, 3) #TODO vedi ste cazzo di rotazioni
+                    self.sensor_direction = np.dot(self.sensor_direction,self.rot_mat)
+                    self.from_positions = np.tile(self.sensor_position, (4, 1))
+                    self.to_positions = np.array([self.sensor_position + direction * self.max_range[0] for direction in self.sensor_direction])
+                    result =p.rayTestBatch(self.from_positions, self.to_positions)
+                    ##  Hit fraction: {result[2]}")
+                    ##  Hit position: {result[3]}")
+                    closest_hit.append(result[0])
+                        #closest_hit[j][0] = result_list[j][0]
+                    # Se c'è un punto di contatto, ottieni la distanza
+                    if closest_hit[j][0] != -1:
+                        hit_distance = closest_hit[j][2]*self.max_range[0]
+                        hit_point = closest_hit[j][3]
+                        # Aggiungi la distanza rilevata come osservazione
+                        observation[i][j]= hit_distance
+                        Hit_point.append(hit_point)
+                    else:
+                        # Se non ci sono oggetti rilevati, assegna una distanza massima
+                        observation[i][j] =  self.max_range[0]
+                        #Hit_point.append([])
+        return observation, Hit_point
+        
+    ################################################################################
+
+    def _getDroneImages(self,
+                        nth_drone,
+                        segmentation: bool=True
+                        ):
+        """Returns camera captures from the n-th drone POV (camera pointing downwards).
+
+        Parameters
+        ----------
+        nth_drone : int
+            The ordinal number/position of the desired drone in list self.DRONE_IDS.
+        segmentation : bool, optional
+            Whehter to compute the compute the segmentation mask.
+            It affects performance.
+
+        Returns
+        -------
+        ndarray 
+            (h, w, 4)-shaped array of uint8's containing the RBG(A) image captured from the n-th drone's POV.
+        ndarray
+            (h, w)-shaped array of uint8's containing the depth image captured from the n-th drone's POV.
+        ndarray
+            (h, w)-shaped array of uint8's containing the segmentation image captured from the n-th drone's POV.
+
+        """
+        if self.IMG_RES is None:
+            print("[ERROR] in BaseAviary._getDroneImages(), remember to set self.IMG_RES to np.array([width, height])")
+            exit()
+        rot_mat = np.array(p.getMatrixFromQuaternion(self.quat[nth_drone, :])).reshape(3, 3)
+        #### Set target point, camera view and projection matrices #
+        target = np.dot(rot_mat,np.array([0.1, 0, -2000])) + np.array(self.pos[nth_drone, :]) 
+        DRONE_CAM_VIEW = p.computeViewMatrix(cameraEyePosition=self.pos[nth_drone, :]+np.array([0, 0, self.L-0.2]),
+                                             cameraTargetPosition=target,
+                                             cameraUpVector=[0, 0, 1],
+                                             physicsClientId=self.CLIENT
+                                             )
+        DRONE_CAM_PRO =  p.computeProjectionMatrixFOV(fov=60.0,
+                                                      aspect=1.0,
+                                                      nearVal=self.L,
+                                                      farVal=1000.0
+                                                      )
+        SEG_FLAG = p.ER_SEGMENTATION_MASK_OBJECT_AND_LINKINDEX if segmentation else p.ER_NO_SEGMENTATION_MASK
+        [w, h, rgb, dep, seg] = p.getCameraImage(width=self.IMG_RES[0],
+                                                 height=self.IMG_RES[1],
+                                                 shadow=1,
+                                                 viewMatrix=DRONE_CAM_VIEW,
+                                                 projectionMatrix=DRONE_CAM_PRO,
+                                                 flags=SEG_FLAG,
+                                                 physicsClientId=self.CLIENT
+                                                 )
+        rgb = np.reshape(rgb, (h, w, 4))
+        dep = np.reshape(dep, (h, w))
+        seg = np.reshape(seg, (h, w))
+        return rgb, dep, seg
+    
+    ################################################################################
+
+    def _computeReward(self):
+        """Computes the current reward value(s).
+
+        Unused as this subclass is not meant for reinforcement learning.
+
+        Returns
+        -------
+        int
+            Dummy value.
+
+        """
+        return -1
+
+    ################################################################################
+    
+    def _computeTerminated(self):
+        """Computes the current terminated value(s).
+
+        Unused as this subclass is not meant for reinforcement learning.
+
+        Returns
+        -------
+        bool
+            Dummy value.
+
+        """
+        return False
+    
+    ################################################################################
+    
+    def _computeTruncated(self):
+        """Computes the current truncated value(s).
+
+        Unused as this subclass is not meant for reinforcement learning.
+
+        Returns
+        -------
+        bool
+            Dummy value.
+
+        """
+        return False
+
+    ################################################################################
+    
+    def _computeInfo(self):
+        """Computes the current info dict(s).
+
+        Unused as this subclass is not meant for reinforcement learning.
+
+        Returns
+        -------
+        dict[str, int]
+            Dummy value.
+
+        """
+        return {"answer": 42} #### Calculated by the Deep Thought supercomputer in 7.5M years
+
+    def _storeHitPoints(self,
+                        nth_drone):
+        """Saves Cloud of points from Hit_point 
+
+        Returns
+        -------
+        QUALCOSA
+        """
+        return {"answer": 42}
---
+++ project/scripts/topo.py
@@ -0,0 +1,160 @@
+import os
+import time
+from datetime import datetime
+import argparse
+import gymnasium as gym
+import numpy as np
+import csv
+
+from gym_pybullet_drones.utils.Logger import Logger
+from project.envs.TopoAviary import TopoAviary
+from gym_pybullet_drones.control.DSLPIDControl import DSLPIDControl
+from gym_pybullet_drones.utils.utils import sync, str2bool
+from gym_pybullet_drones.utils.enums import DroneModel, Physics, ActionType, ObservationType
+
+DEFAULT_GUI = True
+DEFAULT_SAVE_IMAGES = True # True per salvare foto
+DEFAULT_OUTPUT_FOLDER = 'results'
+DEFAULT_COLAB = True
+
+DEFAULT_OBS = ObservationType('rgb') # 'kin' or 'rgb'
+DEFAULT_ACT = ActionType('one_d_rpm') # 'rpm' or 'pid' or 'vel' or 'one_d_rpm' or 'one_d_pid'
+DEFAULT_AGENTS = 2
+
+DEFAULT_DRONES = DroneModel("cf2x")
+DEFAULT_PHYSICS = Physics("pyb")
+DEFAULT_GUI = True
+DEFAULT_PLOT = True
+DEFAULT_SIMULATION_FREQ_HZ = 100
+DEFAULT_CONTROL_FREQ_HZ = 100
+DEFAULT_DURATION_SEC = 30
+DEFAULT_NUM_DRONES = 1
+DEFAULT_IMG_RES = np.array([64, 48])
+
+DEFAULT_LABYRINTH_ID = "2t"  # "0" per i 4 oggettini di BaseRLAviary, "lettera" della versione del labirinto 
+
+def run(
+        output_folder=DEFAULT_OUTPUT_FOLDER,
+        colab=DEFAULT_COLAB, 
+        record_video=DEFAULT_SAVE_IMAGES, 
+        local=True,
+        drone=DEFAULT_DRONES,
+        num_drones=DEFAULT_NUM_DRONES,
+        physics=DEFAULT_PHYSICS,
+        gui=DEFAULT_GUI,
+        plot=DEFAULT_PLOT,
+        simulation_freq_hz=DEFAULT_SIMULATION_FREQ_HZ,
+        control_freq_hz=DEFAULT_CONTROL_FREQ_HZ,
+        duration_sec=DEFAULT_DURATION_SEC,
+        labyrinth_id=DEFAULT_LABYRINTH_ID,
+        ):
+    
+    ### definisci le posizioni iniziali dei droni
+
+    INIT_XYZ = np.array([[3, 0, 1] for i in range(1,num_drones+1)])
+    INIT_RPY = np.array([[.0, .0, .0] for _ in range(num_drones)])
+    env = TopoAviary(drone_model=drone,
+                     num_drones=num_drones,
+                     initial_xyzs=INIT_XYZ,
+                     initial_rpys=INIT_RPY,
+                     physics=physics,
+                     pyb_freq=simulation_freq_hz,
+                     ctrl_freq=control_freq_hz,
+                     gui=gui,   
+                     record=DEFAULT_SAVE_IMAGES,
+                     labyrinth_id=labyrinth_id,
+                     obs=DEFAULT_OBS,    
+                     img_res=DEFAULT_IMG_RES                
+                     )
+    
+    #### Initialize the controllers ############################
+    ctrl = [DSLPIDControl(drone_model=drone) for i in range(num_drones)]
+
+    #### Obtain the PyBullet Client ID from the environment ####
+    PYB_CLIENT = env.getPyBulletClient()
+    DRONE_IDS = env.getDroneIds()
+    
+    #### Initialize the logger #################################
+    logger = Logger(logging_freq_hz=control_freq_hz,
+                    num_drones=num_drones,
+                    output_folder=output_folder,
+                    colab=colab
+                    )
+    
+    #### Run the simulation ####################################
+    action = np.zeros((num_drones,4))
+    START = time.time()
+
+    for i in range(0, int(duration_sec*env.CTRL_FREQ)):
+
+        #### Step the simulation ###################################
+        obs, reward, terminated, truncated, info = env.step(action)
+
+        #### Compute control for the current way point #############
+        # target_pos, target_psy = env.wallFollowing(state)
+        
+        #wall follow che determina target pos e target rpy che verrà poi elaborata dal controllo (ctrl[j].computeControlFromState)
+
+        for j in range(num_drones):
+            action[j, :], _, _ = ctrl[j].computeControlFromState(control_timestep=env.CTRL_TIMESTEP,
+                                                                    state=obs[j],
+                                                                    target_pos=INIT_XYZ[j, :],
+                                                                    target_rpy=INIT_RPY[j, :]
+                                                                    )
+        '''
+        print('PRINTANDO obs')
+        print(obs)
+        print('PRINTANDO obs.shape')
+        print(obs.shape)
+        print('PRINTANDO obs[0]')
+        state = obs[0]
+        print(state)
+        '''
+
+        #### Log the simulation ####################################
+        for j in range(num_drones):
+            logger.log(drone=j,
+                       timestamp=i/env.CTRL_FREQ,
+                       state=obs[j],
+                       # control=np.hstack([TARGET_POS[wp_counters[j], 0:2], INIT_XYZS[j, 2], INIT_RPYS[j, :], np.zeros(6)])
+                       # control=np.hstack([INIT_XYZS[j, :]+TARGET_POS[wp_counters[j], :], INIT_RPYS[j, :], np.zeros(6)])
+                       )
+        
+
+      
+
+        #### Printout ##############################################
+        env.render()
+
+        #### Sync the simulation ###################################
+        if gui:
+            sync(i, START, env.CTRL_TIMESTEP)
+
+    #### Close the environment #################################
+    env.close()
+
+    #### Save the simulation results ###########################
+    logger.save()
+    logger.save_as_csv("camera_tests") # Optional CSV save
+
+    #### Plot the simulation results ###########################
+    if plot:
+        logger.plot()
+    
+if __name__ == "__main__":
+    #### Define and parse (optional) arguments for the script ##
+    parser = argparse.ArgumentParser(description='Helix flight script using CtrlAviary and DSLPIDControl')
+    parser.add_argument('--drone',              default=DEFAULT_DRONES,     type=DroneModel,    help='Drone model (default: CF2X)', metavar='', choices=DroneModel)
+    parser.add_argument('--num_drones',         default=DEFAULT_NUM_DRONES,          type=int,           help='Number of drones (default: 3)', metavar='')
+    parser.add_argument('--physics',            default=DEFAULT_PHYSICS,      type=Physics,       help='Physics updates (default: PYB)', metavar='', choices=Physics)
+    parser.add_argument('--gui',                default=DEFAULT_GUI,       type=str2bool,      help='Whether to use PyBullet GUI (default: True)', metavar='')
+    parser.add_argument('--record_video',       default=DEFAULT_SAVE_IMAGES,      type=str2bool,      help='Whether to record a video (default: False)', metavar='')
+    parser.add_argument('--plot',               default=DEFAULT_PLOT,       type=str2bool,      help='Whether to plot the simulation results (default: True)', metavar='')
+    parser.add_argument('--simulation_freq_hz', default=DEFAULT_SIMULATION_FREQ_HZ,        type=int,           help='Simulation frequency in Hz (default: 240)', metavar='')
+    parser.add_argument('--control_freq_hz',    default=DEFAULT_CONTROL_FREQ_HZ,         type=int,           help='Control frequency in Hz (default: 48)', metavar='')
+    parser.add_argument('--duration_sec',       default=DEFAULT_DURATION_SEC,         type=int,           help='Duration of the simulation in seconds (default: 5)', metavar='')
+    parser.add_argument('--output_folder',     default=DEFAULT_OUTPUT_FOLDER, type=str,           help='Folder where to save logs (default: "results")', metavar='')
+    parser.add_argument('--colab',              default=DEFAULT_COLAB, type=bool,           help='Whether example is being run by a notebook (default: "False")', metavar='')
+    ARGS = parser.parse_args()
+
+    run(**vars(ARGS))
